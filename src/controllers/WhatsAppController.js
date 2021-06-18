f1e40d4d79d9d73143acf26a9bf7a5eb681b64c7
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-06-17 22:05:48 -0300

    Feat: Added listener addContact, talks and menuAttach

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -1,23 +1,23 @@
 
-class WhatsAppController{
+class WhatsAppController {
 
-    constructor(){
+    constructor() {
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
     }
 
-    initEvents(){
+    initEvents() {
         // UNS DOS MÉTOOS PRINCIPAIS RESPONSAVEL PELA INSTANCIA DE 
         // EVENTOS EM DIVERSOS ELEMENTOS.
-
         this.el.myPhoto.on("click", event => {
             this.closeAllLeftPanel()
             this.el.panelEditProfile.show()
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.el.panelEditProfile.addClass("open")
             }, 300)
         })
+
         this.el.btnNewContact.on("click", event => {
             this.closeAllLeftPanel()
             this.el.panelAddContact.show()
@@ -25,10 +25,12 @@ class WhatsAppController{
                 this.el.panelAddContact.addClass("open")
             }, 300)
         })
-        this.el.btnClosePanelEditProfile.on("click", event =>{
+
+        this.el.btnClosePanelEditProfile.on("click", event => {
             this.el.panelEditProfile.removeClass("open")
         })
-        this.el.btnClosePanelAddContact.on("click", event =>{
+
+        this.el.btnClosePanelAddContact.on("click", event => {
             this.el.panelAddContact.removeClass("open")
         })
 
@@ -40,17 +42,58 @@ class WhatsAppController{
             if (event.key === "Enter") {
                 event.preventDefault();
                 this.el.btnSavePanelEditProfile.click()
+            }
+        })
+
+        this.el.btnSavePanelEditProfile.on("click", event => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        })
+
+        this.el.formPanelAddContact.on("submit", event => {
+            event.preventDefault();
+            console.log(this.el.formPanelAddContact.toJSON())
+            this.el.formPanelAddContact.reset();
+        })
+
+        this.el.contactsMessagesList.querySelectorAll(".contact-item").forEach(message => {
+            message.on("click", () => {
+                this.el.home.hide()
+                this.el.main.css({
+                    display: 'flex'
+                })
+            })
+        })
+
+        this.el.btnAttach.on("click", (event) => {
+            event.stopPropagation()
+            this.el.menuAttach.addClass("open")
+            document.addEventListener("click", this.closeMenuAttach.bind(this))
+        })
+
+        // this.btnAttachPhoto.on("click", () => { })
+
+        // this.btnAttachDocument.on("click", () => { })
+
+        // this.btnAttachCamera.on("click", () => { })
+
+        // this.btnAttachContact.on("click", () => { })
+
     }
 
-    closeAllLeftPanel(){
+    closeMenuAttach(event) {
+        document.removeEventListener("click", this.closeAllLeftPanel)
+        this.el.menuAttach.removeClass("open")
+    }
+
+    closeAllLeftPanel() {
         // MÉTODO RESPONSAVEL POR FECHAR TODOS OS PAINEIS LATERAIS
         // POSSIBILITANDO A ABERTURA DE OUTRO PAINEL.
 
         this.el.panelEditProfile.hide()
-        this.el.panelEditProfile.hide()
+        this.el.panelAddContact.hide()
     }
 
-    loadElements(){
+    loadElements() {
         // MÉTODO RESPONSAVEL POR ESTANCIAR TODOS OS ELEMENTOS QUE
         // TIVERAM ID NO FORMATO CAMELCASE. TODOS OS ELEMENTOS FICAM 
         // DISPONIVEIS EM app.el
@@ -61,49 +104,70 @@ class WhatsAppController{
         });
     }
 
-    elementsPrototype(){
+    elementsPrototype() {
         // MÉTODO RESPONSAVEL POR CRIAR METODOS NO PROTOTYPE NO JS
         // PARA SIMPLIFICAÇÃO E RAPIDEZ DO CÓDIGO.
 
-        Element.prototype.hide = function(){
+        Element.prototype.hide = function () {
             this.style.display = "none"
             return this;
         }
-        Element.prototype.show = function(){
+
+        Element.prototype.show = function () {
             this.style.display = "block"
             return this;
         }
-        Element.prototype.toggle = function(){
+
+        Element.prototype.toggle = function () {
             this.style.display = (this.style.display === "none") ? "block" : "none";
             return this;
         }
-        Element.prototype.on = function(events, fn){
+
+        Element.prototype.on = function (events, fn) {
             const eventList = events.split(" ");
             eventList.forEach(event => {
                 this.addEventListener(event, fn);
             })
             return this;
         }
-        Element.prototype.css = function(styles){
-            for(let name in styles){
+
+        Element.prototype.css = function (styles) {
+            for (let name in styles) {
                 this.style[name] = styles[name]
             }
             return this;
         }
-        Element.prototype.addClass = function(className){
+
+        Element.prototype.addClass = function (className) {
             this.classList.add(className);
             return this;
         }
-        Element.prototype.removeClass = function(className){
+
+        Element.prototype.removeClass = function (className) {
             this.classList.remove(className);
             return this;
         }
-        Element.prototype.toggleClass = function(className){
+
+        Element.prototype.toggleClass = function (className) {
             this.classList.toggle(className);
             return this;
         }
-        Element.prototype.hasClass = function(className){
+
+        Element.prototype.hasClass = function (className) {
             return this.classList.contains(className);
         }
+
+        HTMLFormElement.prototype.getForm = function () {
+            return new FormData(this)
+        }
+
+        HTMLFormElement.prototype.toJSON = function () {
+            const json = {}
+            this.getForm().forEach((value, key) => {
+                json[key] = value
+            })
+            return json
+        }
+
     }
 }
