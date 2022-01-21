f080b5f9a4b98b4d32b1a9970736b33680b13e29
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-01-20 22:06:19 -0300

    Feat: Added emoji in input text

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -172,8 +172,21 @@ class WhatsAppController {
         this.el.panelEmojis.querySelectorAll(".emojik").forEach((emoji) => {
             emoji.on("click", (event) => {
                 console.log(event.target);
+                const img = this.el.imgEmojiDefault.cloneNode();
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name => {
+                    img.classList.add(name);
+                });
+
+                this.el.inputText.appendChild(img);
+                this.el.inputText.dispatchEvent(new Event("keyup"));
+
             });
         });
+
     }
 
     startRecordMicrophoneTime() {
